bc7c04ec4551346ac5328e488f7b5b3353c32997
unknown <nadaalbaadani669@gmail.com>
2025-09-15 22:06:07 +0300

    updating deminsions in mobile devices

--- resources/views/layouts/auth2.blade.php
+++ resources/views/layouts/auth2.blade.php
@@ -39,8 +39,8 @@
   align-items: center;
   justify-content: center;
   text-align: center;
-  padding-bottom: 130px;
-  overflow-x: hidden;
+  /* padding-bottom: 130px; */
+  /* overflow-x: hidden; */
 }
 
 @keyframes moveBackground {
@@ -70,6 +70,7 @@
       font-weight: 600;
       color: var(--white);
       margin-bottom: 1rem;
+      margin-top:0.5rem;
     }
 
     .description {
@@ -172,7 +173,8 @@
     .main-wrapper{
       display: flex;
       align-items: center;
-      margin-top : 8rem;
+      /* margin-top : 8rem; */
+      margin: auto auto;
       height : 95dvh;
       background: rgba(255, 255, 255, 0.12);
       backdrop-filter: blur(12px);
@@ -224,7 +226,7 @@
     
     @media (max-width: 768px) {
       .main-wrapper{ width:97%; }
-      .title { font-size: 1.5rem; margin-top:-0.5rem }
+      .title { font-size: 1.5rem;}
       .tagline { font-size: 0.9rem; margin-bottom : 0.75rem; }
       /* .description { font-size: 0.75rem; } */
       .btn { font-size: 0.95rem; padding: 0.7rem 1.6rem; }
@@ -236,6 +238,9 @@
     /* for tablet */
     @media (max-width: 1025px) {
       .main-wrapper{ width : 90%; }
+      .input-icon input { padding : 0.5rem 1rem !important;}
+      .btn-submit{ padding : 0.5rem !important;}
+      .register-link{ font-size:0.75rem !important;}
       .logo{ width: 200px; }
       .title { font-size: 1.5rem; }
       .description { font-size: 0.75rem; }
